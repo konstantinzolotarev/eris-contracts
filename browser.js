8ef1b85835d608c0ee0139cff5336eaef3fdd9e8
Konstantin Zolotarev <konstantin.zolotarev@pixster.eu>
2017-01-23 18:02:54 +0200

    fix naming

--- browser.js
+++ browser.js
@@ -1,5 +1,5 @@
 'use strict'
 
-const ErisDBFactory = require('./index')
+const ErisContractManager = require('./index')
 
-global.ErisDBFactory = ErisDBFactory
+global.ErisContractManager = ErisContractManager
